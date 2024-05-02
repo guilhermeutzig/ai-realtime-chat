1bd117de0ab431e67bdd1da17e956efb9d030093
Guilherme Utzig <36337530+guilhermeutzig@users.noreply.github.com>
2024-05-02 19:58:55 -0300

    fixed styles; added lucide-react for icons

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -2,6 +2,7 @@ import GoogleSignInButton from "@/components/google-sign-in";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { getServerAuthSession } from "@/server/auth";
 import { redirect } from "next/navigation";
+import { LogIn } from "lucide-react";
 
 export default async function SignIn() {
   const session = await getServerAuthSession();
@@ -11,7 +12,9 @@ export default async function SignIn() {
   return (
     <Card className="w-[300px] p-4 text-center">
       <CardHeader>
-        <CardTitle>Sign In</CardTitle>
+        <CardTitle className="flex items-center justify-center gap-4">
+          Sign In <LogIn />
+        </CardTitle>
       </CardHeader>
       <CardContent>
         <GoogleSignInButton />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,15 +19,39 @@ const config = {
     },
     extend: {
       colors: {
-        "primary-foreground": "var(--primary-foreground)",
-        "secondary-foreground": "var(--secondary-foreground)",
+        border: "var(--border)",
+        input: "var(--input)",
+        ring: "var(--ring)",
         background: "var(--background)",
-        card: "var(--card)",
-        primary: "var(--primary)",
-        secondary: "var(--secondary)",
-        alternate: "var(--alternate)",
-        success: "var(--success)",
-        danger: "var(--danger)",
+        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "var(--primary)",
+          foreground: "var(--primary-foreground)",
+        },
+        secondary: {
+          DEFAULT: "var(--secondary)",
+          foreground: "var(--secondary-foreground)",
+        },
+        destructive: {
+          DEFAULT: "var(--destructive)",
+          foreground: "var(--destructive-foreground)",
+        },
+        muted: {
+          DEFAULT: "var(--muted)",
+          foreground: "var(--muted-foreground)",
+        },
+        accent: {
+          DEFAULT: "var(--accent)",
+          foreground: "var(--accent-foreground)",
+        },
+        popover: {
+          DEFAULT: "var(--popover)",
+          foreground: "var(--popover-foreground)",
+        },
+        card: {
+          DEFAULT: "var(--card)",
+          foreground: "var(--card-foreground)",
+        },
       },
       borderRadius: {
         lg: "var(--radius)",
